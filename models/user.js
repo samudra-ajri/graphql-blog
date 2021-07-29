4c31c4f490c805839d07b0f4fa8328f1baae7b97
samudra-ajri <samudra_ajri@live.com>
2021-07-29 22:54:33 +0700

    Add db models and graphql schema

---
+++ models/user.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        default: 'new'
+    },
+    posts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Post'
+        }
+    ]
+});
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User
