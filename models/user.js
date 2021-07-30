41504a4eaebf4e0c61ff13390fa7057877339bee
samudra-ajri <samudra_ajri@live.com>
2021-07-30 13:23:56 +0700

    Add checking password middleware

--- models/user.js
+++ models/user.js
@@ -29,6 +29,10 @@ const userSchema = new Schema({
     ]
 });
 
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
 userSchema.pre('save', async function(next) {
     if (!this.isModified('password')) {
         next()
