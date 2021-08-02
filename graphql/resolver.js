68c24b04999aa3c6bc40d0218a30eb3572f9931a
samudra-ajri <samudra_ajri@live.com>
2021-08-02 09:32:25 +0700

    Add show user profile and update user status

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -217,5 +217,39 @@ export default {
         await user.save();
 
         return true
+    },
+    user: async ({ status }, req) => {
+        if (!req.isAuth) {
+            const error = new Error('Not authenticated!')
+            error.code = 401
+            throw error
+        }
+
+        const user = await User.findById(req.userId)
+        if (!user) {
+            const error = new Error('No user found!')
+            error.code = 404;
+            throw error;
+        }
+
+        return { ...user._doc, _id: user.id.toString() }
+    },
+    updateStatus: async ({ status }, req) => {
+        if (!req.isAuth) {
+            const error = new Error('Not authenticated!')
+            error.code = 401
+            throw error
+        }
+
+        const user = await User.findById(req.userId)
+        if (!user) {
+            const error = new Error('No user found!')
+            error.code = 404
+            throw error
+        }
+        user.status = status
+        await user.save()
+        
+        return { ...user._doc, _id: user._id.toString() }
     }
 }
