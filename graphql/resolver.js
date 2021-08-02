7be70cbbd64a5e9d68e97ea63feaeeaf83b7d0b5
samudra-ajri <samudra_ajri@live.com>
2021-08-02 09:07:34 +0700

    Add updae post functionality

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -138,6 +138,59 @@ export default {
             _id: post._id.toString()
         }
     },
+    updatePost: async function({ id, postInput }, req) {
+        const { title, imageUrl, content } = postInput
+        
+        if (!req.isAuth) {
+            const error = new Error('Not authenticated!');
+            error.code = 401;
+            throw error;
+        }
+
+        const post = await Post.findById(id).populate('creator');
+        if (!post) {
+            const error = new Error('No post found!')
+            error.code = 404
+            throw error
+        }
+
+        if (post.creator._id.toString() !== req.userId.toString()) {
+            const error = new Error('Not authorized!')
+            error.code = 403
+            throw error
+        }
+
+        const errors = [];
+        if (
+            validator.isEmpty(title) ||
+            !validator.isLength(title, { min: 5 })
+        ) {
+            errors.push({ message: 'Title is invalid.' })
+        }
+        if (
+            validator.isEmpty(content) ||
+            !validator.isLength(content, { min: 5 })
+        ) {
+            errors.push({ message: 'Content is invalid.' })
+        }
+        if (errors.length > 0) {
+            const error = new Error('Invalid input.')
+            error.data = errors
+            error.code = 422
+            throw error
+        }
+            post.title = title
+            post.content = content
+            if (imageUrl !== 'undefined') {
+            post.imageUrl = imageUrl
+        }
+
+        const updatedPost = await post.save()
+        return {
+            ...updatedPost._doc,
+            _id: updatedPost._id.toString()
+        }
+    },
     deletePost: async ({ id }, req) => {
         if (!req.isAuth) {
             const error = new Error('Not authenticated!')
